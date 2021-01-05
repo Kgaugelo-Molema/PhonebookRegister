b12f20489addd6500b3f340a2500e221044fdddf
Kgaugelo Molema <kmolema@gmail.com>
2021-01-05 15:52:45 +0200

    Add PhonebookController

--- PhonebookWebApi/Controllers/PhonebookController.cs
+++ PhonebookWebApi/Controllers/PhonebookController.cs
@@ -9,16 +9,16 @@ namespace PhonebookWebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class PhonebookController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<PhonebookController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public PhonebookController(ILogger<PhonebookController> logger)
         {
             _logger = logger;
         }
