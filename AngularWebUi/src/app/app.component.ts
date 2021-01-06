2dc3d6aa750dfbce20924c43625eb837efabc5ba
Kgaugelo Molema <kmolema@gmail.com>
2021-01-06 15:11:47 +0200

    Add posting function

--- AngularWebUi/src/app/app.component.ts
+++ AngularWebUi/src/app/app.component.ts
@@ -12,7 +12,11 @@ export class AppComponent implements OnInit {
   options: any;
 
   private entries: any;
+  private baseUrl = 'api/phonebook';
   searchInput: string;
+  private nameInput: string;
+  private bookInput: string;
+  private numberInput: string;
 
   constructor(private http: HttpClient) {
     this.headers = new HttpHeaders({ 'Content-Type': 'application/json', Accept: 'q=0.8;application/json;q=0.9' });
@@ -24,8 +28,7 @@ export class AppComponent implements OnInit {
 
   search() {
     const promise = new Promise((resolve, reject) => {
-      const apiURL = 'api/phonebook';
-      this.http.get(apiURL)
+      this.http.get(this.baseUrl)
         .toPromise()
         .then(
           res => { // Success
@@ -38,16 +41,37 @@ export class AppComponent implements OnInit {
     return promise;
   }
 
+  addEntry() {
+    const promise = new Promise((resolve, reject) => {
+      const apiURL = `${this.baseUrl}/${this.nameInput}/${this.numberInput}/${this.bookInput}`;
+      this.http.post(apiURL, null)
+        .toPromise()
+        .then(
+          res => { // Success
+            console.log(res);
+            this.search();
+            resolve();
+          }
+        );
+    });
+    return promise;
+  }
+
   private populateResults(res) {
     this.entries = [];
     if (this.searchInput === '' || this.searchInput === undefined) {
       this.entries = res;
     } else {
-      (res as []).forEach(item => {
-        if (item.phonebook.toLowerCase() === this.searchInput.toLowerCase()) {
-          this.entries.push(item);
-        }
-      });
+      this.filterResults(res);
     }
+  }
+
+  private filterResults(res) {
+    (res as []).forEach(item => {
+      if (item.phonebook.toLowerCase() === this.searchInput.toLowerCase() ||
+      (item.name.toLowerCase() === this.searchInput.toLowerCase())) {
+        this.entries.push(item);
+      }
+    });
   }
 }
