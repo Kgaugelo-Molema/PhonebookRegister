7c00879eb539c8512ff7c4afdab5a88cb6851f4a
Kgaugelo Molema <kmolema@gmail.com>
2021-01-06 17:39:51 +0200

    Remove empty folders and commented code

--- PhonebookUI/Program.cs
+++ PhonebookUI/Program.cs
@@ -16,33 +16,13 @@ namespace PhonebookUI
         {
             using (var client = new HttpClient())
             {
-                //client.BaseAddress = new Uri("http://localhost:62437/phonebook");
-                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth","xyz");
-                //client.DefaultRequestHeaders.Accept.Clear();
-                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue());
-
                 Console.WriteLine("Get");
                 HttpResponseMessage response = await client.GetAsync("http://localhost:5000/phonebook");
                 if(response.IsSuccessStatusCode)
                 {
                     var data = await response.Content.ReadAsStringAsync();
                     Console.WriteLine(data);
-                    //Console.WriteLine("Name: " + sites.name + "," + "Year: " + sites.yearInscribed);
                 }
-
-                Console.WriteLine("Post");
-
-                /*
-                Sites newsite = new Sites();
-                newsite.name = "Ryan";
-                newsite.yearInscribed = "2019";
-                response = await client.PostAsJsonAsync("api/sites", newsite);
-                if(response.IsSuccessStatusCode)
-                {
-                    Uri siteUrl = response.Headers.Location;
-                    Console.WriteLine(siteUrl);
-                }
-                */
             }
         }
     }
