df9d52dcce009516eea67abbb830cd61c914ce8d
Kgaugelo Molema <kmolema@gmail.com>
2021-01-06 13:55:25 +0200

    Add Angular UI project

---
+++ AngularWebUi/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'AngularWebUi';
+  headers: HttpHeaders;
+  options: any;
+
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/json', Accept: 'q=0.8;application/json;q=0.9' });
+    this.options = ({ headers: this.headers });
+  }
+  ngOnInit(): void {
+    this.search('');
+  }
+
+  search(term: string) {
+    const promise = new Promise((resolve, reject) => {
+      const apiURL = 'api/phonebook';
+      this.http.get(apiURL)
+        .toPromise()
+        .then(
+          res => { // Success
+            console.log(res);
+            resolve();
+          }
+        );
+    });
+    return promise;
+  }
+}
