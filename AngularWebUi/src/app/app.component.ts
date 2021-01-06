5e55bc973d9ea3777cac0bcf91e226c787f016cd
Kgaugelo Molema <kmolema@gmail.com>
2021-01-06 14:17:38 +0200

    Add list of entries to UI

--- AngularWebUi/src/app/app.component.ts
+++ AngularWebUi/src/app/app.component.ts
@@ -11,12 +11,15 @@ export class AppComponent implements OnInit {
   headers: HttpHeaders;
   options: any;
 
+  private entries: any;
+
   constructor(private http: HttpClient) {
     this.headers = new HttpHeaders({ 'Content-Type': 'application/json', Accept: 'q=0.8;application/json;q=0.9' });
     this.options = ({ headers: this.headers });
   }
   ngOnInit(): void {
     this.search('');
+    
   }
 
   search(term: string) {
@@ -27,6 +30,7 @@ export class AppComponent implements OnInit {
         .then(
           res => { // Success
             console.log(res);
+            this.entries = res;
             resolve();
           }
         );
